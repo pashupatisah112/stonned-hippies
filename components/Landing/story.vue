de48eefc7518dc26b6ceb2c216d45c78c0530f1d
Pashupati Sah <55347604+pashupatisah112@users.noreply.github.com>
2022-05-16 20:29:49 +0545

    minor update

--- components/Landing/story.vue
+++ components/Landing/story.vue
@@ -9,10 +9,10 @@
                 <p class="text-uppercase para-text">
                     During the late 1960s; the year of the magic bus, there were thousands of counterculture travelers who made their way to Afghanistan. The go-to-teahouse called the Sigis in Kabul was the haven for those who honored music and this is where these seven hippies met and instantly got connected through their love for music.
                 </p>
-                <p class="text-uppercase para-text">
+                <!-- <p class="text-uppercase para-text">
                     Almost six decades down the line, these last remaining hippies from that era have secretly built a community of stoned hippies rooted in their love for music. Having survived half a century long political wretch, the community has decided to recommence as the hippiedom legacy and to resurrect the solana blockchain with 2222 elusive inhabitants.
                 </p>
-                <v-btn class="menu-btn">Connect Wallet</v-btn>
+                <v-btn class="menu-btn">Connect Wallet</v-btn> -->
             </v-col>
             <v-col cols="12" lg="6" md="6" align="center">
                 <v-img :src="require('~/assets/images/Our story.png')" max-width="300"></v-img>
