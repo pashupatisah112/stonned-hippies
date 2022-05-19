c8163215ea0d3551a1ff51ea670e845f595af6d5
Pashupati Sah <55347604+pashupatisah112@users.noreply.github.com>
2022-05-19 13:53:53 +0545

    video size upadte

--- components/Landing/main.vue
+++ components/Landing/main.vue
@@ -1,11 +1,9 @@
 <template>
 <div>
-    <v-card :height="getHeight()">
-        <video autoplay muted loop id="bgVideo" style="max-width:100%">
+        <video autoplay muted loop id="bgVideo" class="mt-n16" style="">
             <source :src="require('~/assets/videos/landing-video.mp4')" type="video/mp4">
             Your browser does not support HTML5 video.
         </video>
-    </v-card>
 
 </div>
 </template>
@@ -15,7 +13,6 @@ export default {
     methods: {
         getHeight() {
             return window.innerHeight - 60
-
         },
     }
 }
@@ -23,11 +20,10 @@ export default {
 
 <style>
 #bgVideo {
-    position: absolute;
+    position: static;
     right: 0;
     bottom: 0;
-    min-width: 100%;
-    min-height: 100%;
+    width: 100%;
 }
 
 .content {
