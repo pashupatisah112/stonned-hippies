6bdef57047fa71cbcc6b6823ed75c018d4b127d5
Pashupati Sah <pashupatisah112@gmail.com>
2022-08-07 18:04:24 +0545

    added medium link

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,37 +1,36 @@
 <template>
-  <div style="background-color: black">
+<div style="background-color: black">
     <v-container>
-      <v-row justify="center">
-        <v-col align="center" cols="12" lg="8" md="10">
-          <p class="text-h6 text-lg-h4">Don't Miss Out, Stay Updated</p>
-          <!-- <v-row no-gutters class="my-16">
+        <v-row justify="center">
+            <v-col align="center" cols="12" lg="8" md="10">
+                <p class="text-h6 text-lg-h4">Don't Miss Out, Stay Updated</p>
+                <!-- <v-row no-gutters class="my-16">
                         <v-text-field rounded dense placeholder="Email Address" background-color="#05061C" solo flat class="mr-2"></v-text-field>
                         <v-btn rounded class="menu-btn">Subscribe</v-btn>
                     </v-row>
                     <small class="mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor<br> rhoncus dolor purus non enim praesent </small> -->
-          <v-row justify="center" no-gutters class="py-10">
-            <v-icon
-              small
-              @click="
+                <v-row justify="center" no-gutters class="py-10">
+                    <v-icon small @click="
                 openLink(
                   'https://twitter.com/StonedHippies?t=EvLv7lx-XqFOU4agae2WAA&s=09'
                 )
-              "
-              >mdi-twitter</v-icon
-            >
-            <v-img :src="require('assets/images/discord.png')" class="mx-2"  @click="openLink('https://discord.gg/FNqSVmzEw3')" max-width="18" height="18"></v-img>
-          </v-row>
-        </v-col>
-      </v-row>
+              ">mdi-twitter</v-icon>
+                    <v-img :src="require('assets/images/discord.png')" class="mx-2" @click="openLink('https://discord.gg/FNqSVmzEw3')" max-width="18" height="18"></v-img>
+                    <v-img :src="require('assets/images/medium.png')" @click="openLink('https://medium.com/stoned-hippies')" max-width="20"></v-img>
+
+                </v-row>
+            </v-col>
+        </v-row>
     </v-container>
-  </div>
+</div>
 </template>
+
 <script>
 export default {
-  methods: {
-    openLink(item) {
-      window.open(item, "_blank");
+    methods: {
+        openLink(item) {
+            window.open(item, "_blank");
+        },
     },
-  },
 };
 </script>
